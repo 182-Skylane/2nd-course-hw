b9497e1a361784cc91c6eacd359dddb837ae2465
182-Skylane <skylane.mail@gmail.com>
2024-10-12 23:39:54 +0300

    homework 2 done

--- hw-2.js
+++ hw-2.js
@@ -2,3 +2,45 @@ let a = 10;
 alert(a);
 a += 10;
 alert(a);
+
+const iphone = 2007;
+alert(iphone);
+
+const JS = 'BRENDAN EICH';
+alert(JS);
+
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+let result = 2 ** 5;
+alert(result);
+
+let d = 9;
+let e = 2;
+let f = 9 % 2;
+alert(f);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user = {
+    name: "Paul",
+    age: 39,
+    isAdmin: true
+};
+
+let username = prompt('Введите ваше имя');
+alert(`Привет ${username}!`);
