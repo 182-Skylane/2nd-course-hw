6338c90415706266f948d9d38bbaa41871f680e7
182-Skylane <skylane.mail@gmail.com>
2024-11-04 16:30:20 +0300

    hw-6 is done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Угадай число
 let randomNum = 1 + Math.floor(Math.random() * 100);
 console.log(randomNum);
 
@@ -22,4 +23,46 @@ function guessTheNumber() {
     }
 }
 
+//Простая арфметика
+function generateQuestion() {
+    const operators = ['+', '-', '*', '/'];
+    const num1 = Math.floor(Math.random() * 100);
+    const num2 = Math.floor(Math.random() * 10) + 1; // избежание деления на ноль
+    const operator = operators[Math.floor(Math.random() * operators.length)];
 
+    return { num1, num2, operator };
+}
+
+function calculateAnswer(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+    }
+}
+
+function askQuestion() {
+    const { num1, num2, operator } = generateQuestion();
+    const userAnswer = parseFloat(prompt(`Каков ответ на: ${num1} ${operator} ${num2}?`));
+    const correctAnswer = calculateAnswer(num1, num2, operator);
+
+    if (userAnswer === correctAnswer) {
+        alert("Верно!");
+    } else {
+        alert(`Неверно! Правильный ответ: ${correctAnswer}`);
+    }
+}
+
+//Переверни текст
+function reverseText() {
+    let textRev = prompt('Введите слово');
+    textRev = textRev.split('');
+    textRev.reverse();
+    textRev = textRev.join('');
+    alert(textRev);
+}
